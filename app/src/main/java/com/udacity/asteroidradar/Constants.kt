d60d79b375be5fa3fc88800343cafbd42c6c35d4
Molikkidd <mzkidd5@gmail.com>
2021-12-08 10:04:09 -0800

    add: api and api key for nasa asteroids

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -4,4 +4,6 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
+    const val API_KEY = "oor9Clp2wSaI1eKNJeEx7ZH3BxHEaUm1ijSlNNfE"
+
 }
--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,6 +1,53 @@
 package com.udacity.asteroidradar.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.udacity.asteroidradar.Asteroid
+import com.udacity.asteroidradar.api.AsteroidsApi
+import com.udacity.asteroidradar.api.parseAsteroidsJsonResult
+import kotlinx.coroutines.launch
+import org.json.JSONObject
+//import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.lang.Exception
 
 class MainViewModel : ViewModel() {
+
+//    Encapsulate the asteroid list data
+    private val _asteroidList = MutableLiveData<List<Asteroid>>()
+    val asteroidList: LiveData<List<Asteroid>>
+        get() = _asteroidList
+
+
+    /**
+     * Call getAsteroidData() on init so we can display status immediately.
+     */
+    init {
+        getAsteroidData()
+    }
+    val start_date = "2021-12-07"
+    val end_date = "2021-12-19"
+
+//    grab asteroids from the api and attach to live data variable list
+    private fun getAsteroidData() {
+        viewModelScope.launch {
+            try {
+//                pass in current date and api key
+               val rowListResult = AsteroidsApi.retrofitService.getAsteroids(start_date,end_date)
+                _asteroidList.value = parseAsteroidsJsonResult(JSONObject(rowListResult)).toList()
+//                _asteroidList.value = asteroidList.value
+
+            } catch (e: Exception) {
+                val rowResult = "Failure: " + e.message
+                _asteroidList.value = listOf()
+
+            }
+
+
+        }
+    }
 }
